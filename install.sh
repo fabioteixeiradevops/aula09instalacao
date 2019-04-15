b5830adcf74a9aa56107c40668cd3057c8538c00
Fabio Teixeira <47948335+fabioteixeiradevops@users.noreply.github.com>
2019-04-15 18:12:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-sudo apt-get install htop
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get --yes --force-yes install htop
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -11,8 +11,8 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes install docker-ce docker-ce-cli containerd.io
 sudo docker run hello-world
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
